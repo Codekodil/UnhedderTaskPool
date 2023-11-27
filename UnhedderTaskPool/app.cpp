96d420e0df7d3b88f111c19fc47697b94a01609a
Codekodil <codekodil@gmail.com>
2023-11-27 21:22:43 +0100

    app main

---
+++ UnhedderTaskPool/app.cpp
@@ -0,0 +1,27 @@
+
+#define CPP2_INCLUDE_STD         Yes
+
+//=== Cpp2 type declarations ====================================================
+
+
+#include "cpp2util.h"
+
+#line 1 "app.cpp2"
+
+
+//=== Cpp2 type definitions and function declarations ===========================
+
+#line 1 "app.cpp2"
+
+#line 2 "app.cpp2"
+auto main() -> int;
+
+//=== Cpp2 function definitions =================================================
+
+#line 1 "app.cpp2"
+
+#line 2 "app.cpp2"
+auto main() -> int{
+ std::cout << "main\n";
+}
+
