ef6ffeccf631b1eed8131300483fe1a062829355
Codekodil <codekodil@gmail.com>
2023-12-03 00:54:18 +0100

    delay tests

--- UnitTests/DelayTests.cpp
+++ UnitTests/DelayTests.cpp
@@ -37,8 +37,20 @@ class ExpectingTimer {
 };
 
 auto DelayJoin() -> void;
-#line 32 "DelayTests.cpp2"
+#line 37 "DelayTests.cpp2"
 TEST(DelayTests, DelayJoin)
+
+auto AwaitDelay() -> void;
+#line 54 "DelayTests.cpp2"
+TEST(DelayTests, AwaitDelay)
+
+auto AfterAnother() -> void;
+#line 72 "DelayTests.cpp2"
+TEST(DelayTests, AfterAnother)
+
+auto InParallel() -> void;
+#line 91 "DelayTests.cpp2"
+TEST(DelayTests, InParallel)
 
 
 //=== Cpp2 function definitions =================================================
@@ -64,9 +76,68 @@ TEST(DelayTests, DelayJoin)
 auto DelayJoin() -> void{
  TESTPOOL(2);
 
- auto delayTask {Task<void>::Delay(100ms)}; 
+ cpp2::deferred_init<Task<void>> task; 
 
- ExpectingTimer auto_29_2 {100ms, 10ms}; 
- CPP2_UFCS_0(Join, std::move(delayTask));
+ {
+  ExpectingTimer auto_30_3 {0ms, 5ms}; 
+  task.construct(Task<void>::Delay(100ms));
+ }
+
+ ExpectingTimer auto_34_2 {100ms, 20ms}; 
+ CPP2_UFCS_0(Join, std::move(task.value()));
+}
+
+#line 39 "DelayTests.cpp2"
+auto AwaitDelay() -> void{
+ TESTPOOL(2);
+
+ cpp2::deferred_init<Task<void>> task; 
+
+ {
+  ExpectingTimer auto_45_3 {0ms, 5ms}; 
+  task.construct([]() mutable -> Task<void>{
+   AWAIT(Task<void>::Delay(100ms));
+  }());
+ }
+
+ ExpectingTimer auto_51_2 {100ms, 20ms}; 
+ CPP2_UFCS_0(Join, std::move(task.value()));
+}
+
+#line 56 "DelayTests.cpp2"
+auto AfterAnother() -> void{
+ TESTPOOL(2);
+
+ cpp2::deferred_init<Task<void>> task; 
+
+ {
+  ExpectingTimer auto_62_3 {0ms, 5ms}; 
+  task.construct([]() mutable -> Task<void>{
+   AWAIT(Task<void>::Delay(100ms));
+   AWAIT(Task<void>::Delay(200ms));
+  }());
+ }
+
+ ExpectingTimer auto_69_2 {300ms, 20ms}; 
+ CPP2_UFCS_0(Join, std::move(task.value()));
+}
+
+#line 74 "DelayTests.cpp2"
+auto InParallel() -> void{
+ TESTPOOL(2);
+
+ cpp2::deferred_init<Task<void>> task; 
+
+ {
+  ExpectingTimer auto_80_3 {0ms, 5ms}; 
+  task.construct([]() mutable -> Task<void>{
+   auto t100 {Task<void>::Delay(100ms)}; 
+   AWAIT(Task<void>::Delay(200ms));
+   AWAIT(std::move(t100));
+  }());
+ }
+
+ ExpectingTimer auto_88_2 {200ms, 20ms}; 
+ CPP2_UFCS_0(Join, std::move(task.value()));
 }
 
